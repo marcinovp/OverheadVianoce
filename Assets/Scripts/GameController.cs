bdc229cf9f14b21b29723439f6d63b62d0a081f4
p.marcinov <p.marcinov@gmail.com>
2020-12-19 13:41:01 +0100

    Skip a restart funkcie

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,20 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] Animator cameraAnimator;
     [SerializeField] List<Pismeno> pismena;
 
+    [SerializeField] GameObject skipButton;
+    [SerializeField] GameObject restartButton;
+
     private const string cameraAnimatorParam = "naPozdrav";
 
     void Start()
     {
+        skipButton.SetActive(true);
+        restartButton.SetActive(false);
+
         foreach (Pismeno pismeno in pismena)
         {
             pismeno.OnPismenoDropped += OnPismenoDropped;
         }
+    }
+
+    public void PreskocitHru()
+    {
+        for (int i = pismena.Count - 1; i >= 0; i--)
+        {
+            pismena[i].Drop();
+        }
+    }
+
+    public void HratZnovu()
+    {
+        Scene scene = SceneManager.GetActiveScene(); 
+        SceneManager.LoadScene(scene.name);
     }
 
     private void OnPismenoDropped(Pismeno pismeno)
@@ -24,6 +45,9 @@ public class GameController : MonoBehaviour
         if (pismena.Count == 0)
         {
             GoToPozdrav();
+
+            skipButton.gameObject.SetActive(false);
+            restartButton.gameObject.SetActive(true);
         }
     }
 
