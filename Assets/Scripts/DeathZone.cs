e84ca3708d0ff4214b26ee8853f3c17d6a01801c
p.marcinov <p.marcinov@gmail.com>
2020-12-19 11:07:33 +0100

    Padanie pismen

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Scripts/Pismeno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pismeno : MonoBehaviour
+{
+    public event Action OnPismenoDropped;
+
+    private readonly string droneTag = "Player";
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        HitSomething(collision.collider);
+        Debug.LogFormat("Collision enter");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HitSomething(other);
+        Debug.LogFormat("Trigger enter");
+    }
+
+    private void HitSomething(Collider other)
+    {
+        if (other.CompareTag(droneTag))
+        {
+            rb.useGravity = true;
+            OnPismenoDropped?.Invoke();
+        }
+    }
+}
