4663f6d3fd6e1b5383c898ddbf27b94ce3a3ae3b
p.marcinov <p.marcinov@gmail.com>
2020-12-20 20:57:20 +0100

    Nove GUI

---
+++ Assets/Scripts/InvertedToggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvertedToggle : MonoBehaviour
+{
+    [SerializeField] private GameObject offCheckmark;
+
+    Toggle toggle;
+
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+        OnToggleValueChanged(toggle.isOn);
+    }
+
+    private void OnValidate()
+    {
+        Toggle toggle = GetComponent<Toggle>();
+        OnToggleValueChanged(toggle.isOn);
+    }
+
+    public void OnToggleValueChanged(bool value)
+    {
+        offCheckmark.SetActive(!value);
+    }
+}
